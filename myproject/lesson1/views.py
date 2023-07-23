fa6392969b770a40c3e5cf152c4d2a4982004695
Vladimir Shytsko <shytsko.v@gmail.com>
2023-07-23 10:04:22 +0300

    Add loging

--- myproject/lesson1/views.py
+++ myproject/lesson1/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
+    logger.info("Index page accessed")
     return HttpResponse("Hello, world!")
 
 
 def about(request):
-    return HttpResponse("About us")
+    try:
+        result = 1 / 0
+    except Exception as e:
+        logger.exception(f'Error in about page: {e}')
+        return HttpResponse("Oops, something went wrong.")
+    else:
+        logger.debug('About page accessed')
+        return HttpResponse("This is the about page.")
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -116,3 +116,39 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process} {thread} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': './logs/django.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
+        'lesson1': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
