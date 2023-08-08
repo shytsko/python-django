2d011ee19b7e24171a8dbd5d2b304a4cc28d1fe4
Vladimir Shytsko <shytsko.v@gmail.com>
2023-08-08 17:01:17 +0300

    Update lesson 5

--- myproject/lesson5/admin.py
+++ myproject/lesson5/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'quantity']
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
